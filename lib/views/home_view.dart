da76e5df812711d214552b72c4fbd9d5e8fa7ce5
MohamedIbrahemHamad <mohammedhamad.dev@gmail.com>
2025-09-21 23:48:26 +0300

    feat: add animated text in NoWeatherBody widget using AnimatedTextKit

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -8,7 +8,8 @@ class HomeView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        
+        toolbarHeight: 70,
+        titleSpacing: 35,
         title: Text(
           'Weather by search App',
           style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
--- lib/widgets/no_weather_body.dart
+++ lib/widgets/no_weather_body.dart
@@ -1,26 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
 
 class NoWeatherBody extends StatelessWidget {
   const NoWeatherBody({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: 16),
-      child: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text(
-              'there is no weather 😔 start',
-              style: TextStyle(fontSize: 25, color: Colors.blueGrey),
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          const Text(
+            'there is no weather yet 😔 ',
+            style: TextStyle(
+              fontSize: 25,
+              color: Colors.blueGrey,
+              fontWeight: FontWeight.bold,
             ),
-            Text(
-              'searching now 🔍',
-              style: TextStyle(fontSize: 25, color: Colors.blueGrey),
-            ),
-          ],
-        ),
+          ),
+          AnimatedTextKit(
+            animatedTexts: [
+              TypewriterAnimatedText(
+                'start searching now 🔍',
+                textStyle: TextStyle(
+                  fontSize: 25,
+                  color: Colors.blueGrey,
+                  fontWeight: FontWeight.bold,
+                ),
+                speed: Duration(milliseconds: 100), // typing speed
+              ),
+            ],
+            pause: const Duration(milliseconds: 1000),
+            displayFullTextOnTap: true,
+            stopPauseOnTap: true,
+            repeatForever: true, // show once
+          ),
+        ],
       ),
     );
   }
